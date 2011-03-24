2aa3167f9294e84746ac4c79a7630186f5bfb77d
zhang <zhang@9a4306ee-3332-0410-9a75-a644b53ef5af>
2011-03-24 22:07:20 +0000

    test jquery / add old bookmarks / start using 960.gs for easier layout
    git-svn-id: http://dayabay.ihep.ac.cn/svn/dybsvn/groups/Web/django-sites/odm@11183 9a4306ee-3332-0410-9a75-a644b53ef5af

---
+++ media/js/common.js
@@ -0,0 +1,31 @@
+$("#show_nav").click(function() {
+		$("#nav").toggle("blind", "", "fast");
+	}
+);
+// 
+// $("#search-field").css("color", "#aaa").val(' Run No.');
+// $("#search-field").click(function() {
+//     $(this).val('').css("color", "#333");
+// });
+// 
+// $("#zone-bar li a").click(function() {
+//     var submenu = $(this).parents("li").children("ul");
+//     var hidden = submenu.is(":hidden");
+// 
+//     $("#zone-bar>ul>li>ul").hide();       
+//     $("#zone-bar>ul>li>a").removeClass();
+// 
+//     if (hidden) {
+//         submenu.toggle()
+//         .parents("li").children("a").addClass("zoneCur");
+//     } 
+// });
+// 
+// $("#content, #top-bar").click(function() {
+//     $("#zone-bar>ul>li>ul").hide();       
+//     $("#zone-bar>ul>li>a").removeClass();
+// });
+// 
+// $(".top_menu").click(function() {
+//    return false; 
+// });
