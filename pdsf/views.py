685fe7fa011d15d6e6cc3b31a0ba9ca2ad7d90b3
zhang <zhang@9a4306ee-3332-0410-9a75-a644b53ef5af>
2011-05-06 18:28:43 +0000

    disable IE view PDSF job page. blame NERSC who do not currently support the auth login toolbar in IE
    git-svn-id: http://dayabay.ihep.ac.cn/svn/dybsvn/groups/Web/django-sites/odm@11956 9a4306ee-3332-0410-9a75-a644b53ef5af

--- pdsf/views.py
+++ pdsf/views.py
@@ -1,10 +1,15 @@
 from django.views.generic.simple import direct_to_template
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404
 
 @login_required
 def users(request):
     '''user list'''
-        
+    
+    # blame NERSC who do not currently support IE auth login toolbar 
+    if isIE(request):
+        return HttpResponse('Sorry, your browser is not supported')
+
     return direct_to_template(request, 
         template = 'pdsf/users.html',
         extra_context = {
@@ -14,9 +19,22 @@ def users(request):
 @login_required
 def user(request, uname):
     '''user details'''
+    
+    if isIE(request):
+        return HttpResponse('Sorry, your browser is not supported')
         
     return direct_to_template(request, 
         template = 'pdsf/user.html',
         extra_context = {
             'uname' : uname,
         })
+
+# ======================================================
+def isIE(request):
+    user_agent = request.META.get('HTTP_USER_AGENT')
+    try:
+        user_agent.index('MSIE')
+        return True
+    except ValueError:
+        return False
+    
