94499cf8be99ac07a2f420a800264bf4e549544f
dwebber <dwebber@9a4306ee-3332-0410-9a75-a644b53ef5af>
2012-02-22 17:44:37 +0000

    added temperature readouts.  changed tabs to spaces (oops)
    git-svn-id: http://dayabay.ihep.ac.cn/svn/dybsvn/groups/Web/django-sites/odm@16087 9a4306ee-3332-0410-9a75-a644b53ef5af

--- media/js/dcs/formula.js
+++ media/js/dcs/formula.js
@@ -3,6 +3,7 @@ load_formulas();
 
 // for values, see http://dayabay.ihep.ac.cn/svn/dybaux/AD/ADSensors/LidSensors/DeliveryAndInstallation/master conversion table.ods
 // David Webber Nov 10, 2011
+// Temperature sensor offsets provided by R. Zhao on Feb 22, 2012
 
 function load_formulas(){
     formulas = {
@@ -11,56 +12,72 @@ function load_formulas(){
             ultrasonic_ls:    function(x) { return (2802.2-x)/11.384+23; },
             capacitance_mo:   function(x) { return 240-224.63+x*0.208044; },
             capacitance_gdls: function(x) { return 0.1507*x+0.5525+41; },
-            capacitance_ls:   function(x) { return 0.15015*x+14.2473+23; }
+            capacitance_ls:   function(x) { return 0.15015*x+14.2473+23; },
+            temperature_gdls: function(x) { return x+0.256; },
+            temperature_ls:   function(x) { return x+0.156; }
         },
         Ad2Lidsensor : {
             ultrasonic_gdls:  function(x) { return (2724.7-x)/11.416+41; },
             ultrasonic_ls:    function(x) { return (2786.9-x)/11.638+23; },
             capacitance_mo:   function(x) { return 240-224.24+x*0.208217; },
             capacitance_gdls: function(x) { return 0.15105*x-1.8593+41; },
-            capacitance_ls:   function(x) { return 0.15175*x+15.0732+23; }
+            capacitance_ls:   function(x) { return 0.15175*x+15.0732+23; },
+            temperature_gdls: function(x) { return x-0.026; },
+            temperature_ls:   function(x) { return x+0.13;  }
         },
         Ad3Lidsensor : { 
             ultrasonic_gdls:  function(x) { return (2703.6-x)/11.354+41; },
             ultrasonic_ls:    function(x) { return (2765.9-x)/11.382+23; },
             capacitance_mo:   function(x) { return 240-225.64+x*0.208335; },
             capacitance_gdls: function(x) { return 0.1517*x-3.2797+41; },
-            capacitance_ls:   function(x) { return 0.14785*x-3.6478+23; }
+            capacitance_ls:   function(x) { return 0.14785*x-3.6478+23; },
+            temperature_gdls: function(x) { return x+0.07; },
+            temperature_ls:   function(x) { return x+0.05; }
         },
         Ad4Lidsensor : {
             ultrasonic_gdls:  function(x) { return (2680-x)/11.369+41; },
             ultrasonic_ls:    function(x) { return (2737-x)/11.451+23; },
             capacitance_mo:   function(x) { return 240-225.4+x*0.208438; },
             capacitance_gdls: function(x) { return 0.14815*x-5.88+41; },
-            capacitance_ls:   function(x) { return 0.14830*x+0.77+23; }
+            capacitance_ls:   function(x) { return 0.14830*x+0.77+23; },
+            temperature_gdls: function(x) { return x+0.24;  },
+            temperature_ls:   function(x) { return x+0.005; }
         },
         Ad5Lidsensor : {
             ultrasonic_gdls:  function(x) { return (2694.39-x)/11.334+41; },
             ultrasonic_ls:    function(x) { return (2764.12-x)/11.403+23; },
             capacitance_mo:   function(x) { return 240-226.2+x*0.205716; },
             capacitance_gdls: function(x) { return 0.1452*x+5.15+41; },
-            capacitance_ls:   function(x) { return 0.14790*x-2.0+23; }
+            capacitance_ls:   function(x) { return 0.14790*x-2.0+23; },
+            temperature_gdls: function(x) { return 0.996*x+0.356; },
+            temperature_ls:   function(x) { return 0.992*x+0.451; }
         },
         Ad6Lidsensor : { // TODO: check GdLS, LS capacitance offsets
             ultrasonic_gdls:  function(x) { return (2708.48-x)/11.366+41; },
             ultrasonic_ls:    function(x) { return (2767.67-x)/11.378+23; },
             capacitance_mo:   function(x) { return 240-225.66+x*0.208487; },
             capacitance_gdls: function(x) { return 0.1471*x-1.5+41; },
-            capacitance_ls:   function(x) { return 0.15125*x-4.41+23; }
+            capacitance_ls:   function(x) { return 0.15125*x-4.41+23; },
+            temperature_gdls: function(x) { return 0.997*x+0.283; },
+            temperature_ls:   function(x) { return 0.995*x-0.160; }
         },
         Ad7Lidsensor : { // placeholders only
             ultrasonic_gdls:  function(x) { return -(2720.6-x)/11.454+41; },
             ultrasonic_ls:    function(x) { return -(2802.2-x)/11.384+23; },
             capacitance_mo:   function(x) { return -240-224.63+x*0.208044; },
             capacitance_gdls: function(x) { return -0.1507*x+0.5525+41; },
-            capacitance_ls:   function(x) { return -0.14790*x-0.3816+23; }
+            capacitance_ls:   function(x) { return -0.14790*x-0.3816+23; },
+            temperature_gdls: function(x) { return 0.992*x+0.391; }, // sensor V
+            temperature_ls:   function(x) { return 0.995*x+0.355; }  // sensor v
         },
         Ad8Lidsensor : { // placeholders only
             ultrasonic_gdls:  function(x) { return -(2720.6-x)/11.454+41; },
             ultrasonic_ls:    function(x) { return -(2802.2-x)/11.384+23; },
             capacitance_mo:   function(x) { return -240-224.63+x*0.208044; },
             capacitance_gdls: function(x) { return -0.1507*x+0.5525+41; },
-            capacitance_ls:   function(x) { return -0.15015*x+14.2473+23; }
+            capacitance_ls:   function(x) { return -0.15015*x+14.2473+23; },
+            temperature_gdls: function(x) { return 0.992*x+0.262; }, // sensor Y
+            temperature_ls:   function(x) { return 0.996*x+0.220; }  // sensor y
         }
     };
 }
