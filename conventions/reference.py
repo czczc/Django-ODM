a9d32e524013801f84c6bf8be654ec37fd132481
zhang <zhang@9a4306ee-3332-0410-9a75-a644b53ef5af>
2012-05-17 16:32:16 +0000

    odm: update ref runs as suggested in doc:7953
    git-svn-id: http://dayabay.ihep.ac.cn/svn/dybsvn/groups/Web/django-sites/odm@17053 9a4306ee-3332-0410-9a75-a644b53ef5af

--- conventions/reference.py
+++ conventions/reference.py
@@ -5,7 +5,10 @@ class ReferenceRun(object):
     '''Reference Run'''
     
     StandardRun = {
-        'EH1-Physics' : 17158,
-        'EH2-Physics' : 21361,
-        'EH3-Physics' : 21364,
+        # 'EH1-Physics' : 17158,
+        # 'EH2-Physics' : 21361,
+        # 'EH3-Physics' : 21364,
+        'EH1-Physics' : 23244,
+        'EH2-Physics' : 23257,
+        'EH3-Physics' : 23238,
     }
